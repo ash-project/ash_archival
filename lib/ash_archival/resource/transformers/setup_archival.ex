9a18f4bbf0a5dff5cef1c97ff7fd86377b4c6e30
Leon Qadirie <39130739+leonqadirie@users.noreply.github.com>
2026-02-02 07:05:18 +0100

    chore: SPDX contributor links (#180)

--- lib/ash_archival/resource/transformers/setup_archival.ex
+++ lib/ash_archival/resource/transformers/setup_archival.ex
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2022 ash_archival contributors <https://github.com/ash-project/ash_archival/graphs.contributors>
+# SPDX-FileCopyrightText: 2022 ash_archival contributors <https://github.com/ash-project/ash_archival/graphs/contributors>
 #
 # SPDX-License-Identifier: MIT
 
